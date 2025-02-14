91b753e03252e4be7a5796d09d4a639f4f6c5743
Simran Chawla <sc1236@pleasantonusd.net>
2025-02-14 02:50:57 +0000

    sam

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -15,6 +15,7 @@ public class IrregularPolygon {
     public void add(Point2D.Double aPoint)
     {
         // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint)
     }
 
     public double perimeter() {
@@ -35,6 +36,14 @@ public class IrregularPolygon {
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
             DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
             pen.move(50, 50);
+            Point2D.Double first = myPolygon.get(0);
+            pen.up();
+            pen.move(first.getX(), first.getY());
+            pen.down();
+            for (int i = 1; i < myPolygon.size(); i++) {
+                Point2D.Double after = myPolygon.get(i);
+                pen.move(after.getX(), after.getY());
+            }
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
